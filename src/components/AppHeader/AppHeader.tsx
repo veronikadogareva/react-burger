243439e29a836e1e145af45987965cc35b34efd4
Bogdan3535 <bogdantarasov14@gmail.com>
2024-12-24 18:45:16 +0600

    header1

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import headerStyles from './AppHeader.module.css';
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 export default function AppHeader() {
-
     return (
-        <header>Header</header>
+        <header className={headerStyles.header}>
+            <Logo />
+            <nav className={headerStyles.nav}>
+                    <a className={headerStyles.navLink}><BurgerIcon type="primary" /> Конструктор</a>
+                    <a className={headerStyles.navLink}><ListIcon type="secondary" /> Лента заказов</a>
+                    <a className={headerStyles.navLink}><ProfileIcon type="secondary" /> Личный кабинет</a>         
+            </nav>
+        </header>
     );
 }
